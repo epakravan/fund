9fb6534d53915701d5145c17dfe72f5bb34de851
Eric Pakravan <epakravan@gmail.com>
2014-03-20 17:50:49 -0700

    Create Fund Project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fund::Application.config.secret_key_base = '6fd67a301dad7fd2a2cf83ed2e24815c5876ef07a4fe6d2bdfa581ca098827f22a43e9c478636712e0eff0b52cfc62235af74612d1a8f06c27a978a80d2c5ace'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Fund::Application.config.session_store :cookie_store, key: '_fund_session'
